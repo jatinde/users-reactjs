84deed0732e4188c8ad680567e50bb2f1b52b08f
jatinde <jpsjatinder@gmail.com>
2021-06-29 20:06:08 +0530

    React Users Application Demo for interview

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { UsersTile } from './UsersTile';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { UserDetails } from './UserDetails';
+import React, { useEffect, useState } from 'react';
+import { getUsers } from './api/api';
+export const UserContext = React.createContext();
 function App() {
+  const [users, setUsers] = useState([]);
+    useEffect(() => {
+      getUsers().then(userData => setUsers(userData));
+    }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <Router>
+        <Switch>
+        <UserContext.Provider value={users}>
+          <Route exact={true} path="/">
+              <UsersTile  />
+          </Route>
+          <Route path="/userdetails/:email">
+              <UserDetails  />
+          </Route>
+          </UserContext.Provider>
+        </Switch>
+      </Router>
+     
     </div>
   );
 }
